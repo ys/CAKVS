d3b7208b5303ccb7b967ea561afc577563825330
Yannick <yannick.schutz@gmail.com>
2015-02-10 00:34:27 +0100

    Make it Webscale with webserver

--- env.go
+++ env.go
@@ -3,6 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
@@ -21,9 +25,35 @@ func (m *MongoENV) Set(k string, val *MongoVal) {
 	os.Setenv(k, string(valString))
 }
 
+func (m *MongoENV) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	key := r.URL.Path[len("/"):]
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+
+	if r.Method == "GET" {
+		if err := json.NewEncoder(w).Encode(m.Get(key)); err != nil {
+			panic(err)
+		}
+	}
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+		if err != nil {
+			panic(err)
+		}
+		if err := r.Body.Close(); err != nil {
+			panic(err)
+		}
+		var val MongoVal
+		if err := json.Unmarshal(body, &val); err != nil {
+			panic(err)
+		}
+		fmt.Println(string(body), key, val)
+		m.Set(key, &val)
+	}
+}
+
 func main() {
 	m := MongoENV{}
-	m.Set("LOL", &MongoVal{"YOLO": "TRUEFRIEND"})
-	fmt.Println(m.Get("LOL"))
-	fmt.Println(m.Get("DAT"))
+	http.Handle("/", &m)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
